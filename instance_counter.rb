933e667b11358592803e3fb90b88663a2a5c8eec
Яна Манцевич <36705460+yanamanc@users.noreply.github.com>
2018-11-20 14:26:58 +0300

    Update instance_counter.rb

--- instance_counter.rb
+++ instance_counter.rb
@@ -6,24 +6,21 @@ module InstanceCounter
   end
 
   module ClassMethods
-  attr_accessor :instances
+  attr_reader :instances
   
-<<<<<<< HEAD
     def counter
       @instances ||= 0
       @instances += 1
     end 
     
-=======
-  def counter
-    @instances ||= 0
-    @instances += 1
-  end 
->>>>>>> 56ae5756c4e6b1d708e6eb2b26cb8b54472359fa
+    def counter
+      @instances ||= 0
+      @instances += 1
+    end 
   end
 
   module InstanceMethods
-    extend ClassMethods
+    private
     def register_instance
       self.class.send :counter
     end
